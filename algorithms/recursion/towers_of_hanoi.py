487a7e04548ad8b4b106da5f7ce576b2d76da1ad
novandev <novanadams35@gmail.com>
2020-02-19 23:29:58 -0800

    add/tower of hanoi

--- algorithms/recursion/towers_of_hanoi.py
+++ algorithms/recursion/towers_of_hanoi.py
@@ -4,3 +4,11 @@ Tower of Hanoi problem
 You start off with 3 poles and one of the poles has rings increasing in size in a stack from top to bottom
 
 """
+
+
+
+def tower_of_hanoi_solver(counter: int,start: int, goal: int, temp: int):
+    if counter >= 1:
+        return tower_of_hanoi_solver(counter-1,start,temp,goal)
+        print(f'move {start} => {goal}')
+        return tower_of_hanoi_solver(counter-1,temp,goal,start)
