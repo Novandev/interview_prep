e1dce600489735fa69c6fd14e5eb6ee2aae699cc
novandev <novanadams35@gmail.com>
2020-02-25 14:43:57 -0800

    update/ permutation maker

--- interview_questions/permutation_backtracking.py
+++ interview_questions/permutation_backtracking.py
@@ -10,12 +10,16 @@
 def permutation_maker(a, n, k, depth, used, current_permutation = [], permutations = []):
   '''
   Implement permutation of k items out  of n items
+  
+  a: the list we will make permutations out of
+  n: the length of the list as a sentonel value
   depth: start from 0, and represent the depth of the search
   used: track what items are  in the partial solution from the set of n
-  curr: the current partial solution
-  ans: collect all the valide solutions
+  current_permutation: the current partial solution
+  permutations: collect all the valide solutions
+  k: The sentinel value that tracks how deep the recursion should go
   '''
-  if depth == k: #end condition
+  if depth == k: #if the "depth" is equal to the lenth of the list
     permutations.append(current_permutation[::]) # use deepcopy because current_permutation is tracking all partial solution, it eventually become []
     return
   
@@ -32,13 +36,13 @@ def permutation_maker(a, n, k, depth, used, current_permutation = [], permutatio
       current_permutation.pop()
       print('backtrack: ', current_permutation)
       used[i] = False
-  return permutations
+  return
 
-a = [1, 2, 3]
+a = [1, 2, 3,4]
 n = len(a)
-# ans = [[None]]
+k = len(a)
 # Used is a list that will track wether or not
 used = [False] * len(a)
 ans = []
-permutation_maker(a, n, n, 0, used, [], ans)
+permutation_maker(a, n, k, 0, used, [], ans)
 print(ans)
