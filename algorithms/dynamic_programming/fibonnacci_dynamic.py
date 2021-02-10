27448f1fc6e2944c48ad0dbeee3edc90c2b2e99e
novan-thrivelab <novan@thrivelab.com>
2021-02-09 23:25:22 -0500

    add/ fibination

--- algorithms/dynamic_programming/fibonnacci_dynamic.py
+++ algorithms/dynamic_programming/fibonnacci_dynamic.py
@@ -1,16 +1,52 @@
 """
-
+Dynamic programming and Memoization
 
 """
 
 
-def fibonacciVal(n):
-    memo = [0] * (n+1)
-    memo[0], memo[1] = 0, 1
-    for i in range(2, n+1):
-        memo[i] = memo[i-1] + memo[i-2]
-    # print(memo[n])
-    return memo[n]
+def fibonacci_dynamic(n):
+    '''
+    This function displayes the proper use of memoization 
+    
+    '''
+    pass
 
 
-print(fibonacciVal(5))
+def fibonacci_recursion(n):
+    '''
+        SO recursion is usually a place to start  with this kinda stuff
+        The problem is that it builds a tree so larger values end up being intractable
+
+        fib(7) ={
+            6{
+                5:{
+                    4
+                    3"
+                }
+                4:{
+                    2
+                    3
+                }
+            }
+            5:{
+
+            }
+        }
+
+        This means that it tends to repeat steps, so we'll need to cut down on that
+    
+    This is 2^n
+
+    so fibonacci_recursion(50) == 2^50 YIKES
+    '''
+    print(n-1, n-2)
+    # Base case
+    if n <= 2:
+        return 1
+    
+    return fibonacci_recursion( n - 1) + fibonacci_recursion( n- 2)
+
+
+if __name__ == "__main__":
+
+    print(fibonacci_recursion(7))
